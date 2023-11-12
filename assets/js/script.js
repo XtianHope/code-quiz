93ebbeba491db3eb830622487cce7d413debf7dc
Christian Tino <xtianhope@gmail.com>
2023-11-11 23:00:41 -0500

    added next button to HTML to move user through pages/next question by clicking, included query selector for button in js, added index to keep track of question user is on, added questions and answers to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,58 @@
+// Declaring/selecting HTML elements
 var startScreen = document.querySelector(".start");
 var quizScreen = document.querySelector(".quiz");
 var endScreen = document.querySelector(".end");
 var startButton = document.querySelector("#start-btn");
+var nextButton = document.querySelector("#next-btn");
+var questionContainer = document.querySelector("question-container");
+var questionTitle = document.querySelector("question-title");
+
+//Index to keep track of the current question user is on
+var currentQuestionIndex = 0;
+
+// Questions and Answers
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: ["HyperText Markup Language", "High-Level Text Management Language", "HyperTransfer Markup Language", "HyperText Manipulation Language"],
+    },
+    {
+        question: "What is the purpose of the console.log() function in JavaScript?",
+        answers: ["Display a message in the console", "Log information to the server", "Print content on the webpage", "Define a new variable"],
+    },
+    {
+        question: "How do you declare a variable named count and assign it the value 10?",
+        answers: ["var count = 10;", "variable count = 10;", "count = 10;", "declare count = 10;"],
+    },
+    {
+        question: "What is the result of the expression 5 + "5" in JavaScript?",
+        answers: ["55<", "10", "5", "Error"],
+    },
+    {
+        question: "What does the term "Boolean" represent in JavaScript?",
+        answers: ["A type of loop", "A sequence of characters", "A data type representing true or false", "A function declaration"],
+    },
+    {
+        question: "What is JavaScript used for in web development?",
+        answers: ["Styling web pages", "Adding interactivity to websites", "Creating database tables", "Defining webpage structure"],
+    },
+    {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        answers: ["let", "variable", "var", "const"],
+    },
+    {
+        question: "Which function is used to display a message in a popup box?",
+        answers: ["popup()", "prompt()", "message()", "alert()"],
+    },
+    {
+        question: "Which symbol is used for single-line comments in JavaScript?",
+        answers: ["#", "//", "/", "__"],
+    },
+    {
+        question: "In JavaScript, what does the term "array" represent?",
+        answers: ["A group of related variables", "A type of loop", "A sequence of characters", "A mathematical operation"],
+    },
+]
 
 function showStart() {
     startScreen.style.display = null;
