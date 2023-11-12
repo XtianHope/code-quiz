55ebd2110a3564e4d2e5c50877d4ceed98f246d1
Christian Tino <xtianhope@gmail.com>
2023-11-12 00:12:15 -0500

    updated query selector for the next button to reflect seperate ids for eacfh next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var startScreen = document.querySelector(".start");
 var quizScreen = document.querySelector(".quiz");
 var endScreen = document.querySelector(".end");
 var startButton = document.querySelector("#start-btn");
-var nextButton = document.querySelector("#next-btn");
+var nextButton = document.querySelector(".quiz button[id^="next-btn"]");
 var initialsInput = document.querySelector("initials")
 var submitScoreButton = document.querySelector("#submit-score")
 
