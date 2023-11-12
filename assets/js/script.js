44edd3a01d6661244b61617003ee2295177cc5a4
Christian Tino <xtianhope@gmail.com>
2023-11-11 21:15:57 -0500

    set up functions for page calling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,21 @@ var quizScreen = document.querySelector(".quiz");
 var endScreen = document.querySelector(".end");
 
 function showStart() {
-
+    startScreen.style.display = null;
+    quizScreen.style.display = "none";
+    endScreen.style.display = "none";
 }
 
 function showQuiz() {
-    
+    startScreen.style.display = "none";
+    quizScreen.style.display = null;
+    endScreen.style.display = "none";
 }
 
 function showEnd() {
-    
+    startScreen.style.display = "none";
+    quizScreen.style.display = "none";
+    endScreen.style.display = null;
 }
 
 function init () {
